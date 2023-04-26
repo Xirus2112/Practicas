a65d15ee9d07092689512ea0d74a11110d844199
Carlos Alvarado M <66229925+Xirus2112@users.noreply.github.com>
2023-04-25 22:23:37 -0500

    Aprendiendo sobre las listas

---
+++ Udemy/Python/Nicolas Scurmann/tipos-avanzados/01-listas.py
@@ -0,0 +1,23 @@
+# Creando lista de nuemeros
+
+numeros = [1, 2, 3]
+letras = ["a", "b", "c"]
+palabras = ["chanchito", "feliz"]
+palabras_felices = ["chanchito", "feliz", "Jeremy", "Hijo"]
+booleans = [True, False, True, True]
+matriz = [[0, 1], [1, 0]]
+
+# Colocando 10 ceros
+ceros = [0, 1] * 10
+
+# Alfanumericos
+
+alfanumerico = numeros + letras
+
+# Creando lista con rango de numeros
+rango = list(range(1, 11))
+
+# Creando valores de string
+chars = list("Hola Jeremy")
+
+print(chars)
