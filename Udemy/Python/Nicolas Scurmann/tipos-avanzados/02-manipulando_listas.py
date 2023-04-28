2dcb5c8956af2b9ee0c5a3065b8a2681fb7dc372
Carlos Alvarado M <66229925+Xirus2112@users.noreply.github.com>
2023-04-28 14:09:49 -0500

    Manipulando listas

---
+++ Udemy/Python/Nicolas Scurmann/tipos-avanzados/02-manipulando_listas.py
@@ -0,0 +1,6 @@
+# Modificando listas
+
+nombres = ["Carlos", "Jeremy", "Mathias", "Dilan", "Made", "Mary Luz"]
+
+print(nombres[::2])
+# los : indican el indice en el que empezara a contar y mostrar el dato solicitado a la lista
