e73e7db4422da84011df08647ded4d67b318f04b
Carlos Alvarado M <66229925+Xirus2112@users.noreply.github.com>
2023-09-14 13:19:21 -0500

    Conociendo un poco la sintaxis de Type Script

--- Platzy/Angular/my-store/src/app/recap.ts
+++ Platzy/Angular/my-store/src/app/recap.ts
@@ -2,5 +2,16 @@ const username: string = 'Carlos';
 const sum = (a : number, b : number) =>{
     return a + b;
 }
-
 sum(2,4)
+
+class Person{
+    age: number;
+    lastname: string;
+
+    constructor(age:number, lastname: string){
+        this.age = age;
+        this.lastname = lastname;
+    }
+}
+
+const carlos = new Person(32, 'Alvarado');
